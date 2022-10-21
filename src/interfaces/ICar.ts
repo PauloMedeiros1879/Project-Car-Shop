4925b942fbe440da32741906f3ede6188d1d21c9
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-10-21 15:37:38 -0400

    Feat: Adiciona a interface ICar, com o atributo seatsQty - REQ03

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -11,7 +11,16 @@ export const CarZodSchema = VehicleZodSchema.extend({
     .positive({ message: 'Must be a positive number' })
     .lte(4, { message: 'DoorQty must be less than 4' })
     .gte(2, { message: 'DoorQty must be greater than 2' }),
-
+    
+  seatsQty: z
+    .number({
+      required_error: 'SeatQty is required',
+      invalid_type_error: 'SeatQty must be a number',
+    })
+    .int({ message: 'Must be a integer' })
+    .positive({ message: 'Must be a positive number' })
+    .lte(7, { message: 'SeatQty must be less than 7' })
+    .gte(2, { message: 'SeatQty must be greater than 2' }),
 });
 
 export type ICar = z.infer<typeof CarZodSchema>;
