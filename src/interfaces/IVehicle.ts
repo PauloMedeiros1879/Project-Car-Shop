5b8908ae7b0fe7a9e3029ebd48cc5061401731d2
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-10-21 15:25:03 -0400

    Feat: Adiciona a interface IVehicle, com o atributo year - REQ02

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -10,6 +10,16 @@ export const VehicleZodSchema = z.object({
       message: 'Model must be 3 or more characters long',
     }),
 
+  year: z
+    .number({
+      required_error: 'Year is required',
+      invalid_type_error: 'Year must be a number',
+    })
+    .int({ message: 'Must be a integer' })
+    .positive({ message: 'Must be a positive number' })
+    .lte(2022, { message: 'Year must be less than 2022' })
+    .gte(1900, { message: 'Year must be greater than 1900' }),
+
 });
 
 export type IVehicle = z.infer<typeof VehicleZodSchema>;
