d5f2a3eb149bbeaae34ce46990211fbbd2f67229
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-10-24 12:14:05 -0400

    Feat: Adiciona o model de Car e seus atributos, também adiciona o model do mongo

---
+++ src/models/CarModel.ts
@@ -0,0 +1,22 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const carMongooseSchema = new Schema<ICar>(
+  {
+    model: String,
+    year: Number,
+    color: String,
+    status: { type: Boolean, required: false },
+    buyValue: Number,
+    doorsQty: Number,
+    seatsQty: Number,
+  },
+  { versionKey: false },
+);
+
+export default class CarModel extends MongoModel<ICar> {
+  constructor(model = mongooseCreateModel('Car', carMongooseSchema)) {
+    super(model);
+  }
+}
