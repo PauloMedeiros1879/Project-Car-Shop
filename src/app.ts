c8a052a62fcb974c7e22a6ed790100f5705e06d8
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-10-24 12:53:37 -0400

    Feat: Adiciona a rota cars em app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import carRouter from './routes/CarRouter';
 
 const app = express();
+
+app.use(express.json());
+app.use('/cars', carRouter);
 
 export default app;
