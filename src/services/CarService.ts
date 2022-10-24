1546b3c5f6aa63465c94fc73b5321541bace7051
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-10-24 13:07:03 -0400

    Feat: Adiciona o erros de car na model service

--- src/services/CarService.ts
+++ src/services/CarService.ts
@@ -1,6 +1,7 @@
 import { IService } from '../interfaces/IService';
 import { CarZodSchema, ICar } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../middlewares/errorTypes';
 
 export default class CarService implements IService<ICar> {
   private _car: IModel<ICar>;
@@ -22,7 +23,7 @@ export default class CarService implements IService<ICar> {
 
   public async readOne(_id: string): Promise<ICar> {
     const car = await this._car.readOne(_id);
-    if (!car) throw new Error('InvalidMongoId');
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
     return car;
   }
 
@@ -30,13 +31,13 @@ export default class CarService implements IService<ICar> {
     const parsed = CarZodSchema.safeParse(obj);
     if (!parsed.success) throw parsed.error;
     const carUpdate = await this._car.update(_id, parsed.data);
-    if (!carUpdate) throw new Error('InvalidMongoId');
+    if (!carUpdate) throw new Error(ErrorTypes.EntityNotFound);
     return carUpdate;
   }
 
   public async delete(_id: string): Promise<ICar> {
     const car = await this._car.delete(_id);
-    if (!car) throw new Error('InvalidMongoId');
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
     return car;
   }
 }
