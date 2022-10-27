ae281433bb6f0421856c7c5ac864e0f38cc4f23b
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-10-26 21:38:03 -0400

    Fix: Resolve o problema no arquivo app.ts aonde o /motorcycle era no plural, /motorcycles

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/cars', carRouter);
-app.use('/motorcycle', motorcycleRouterRouter);
+app.use('/motorcycles', motorcycleRouterRouter);
 app.use(errorHandler);
 
 export default app;
