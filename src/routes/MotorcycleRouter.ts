43bd8b5ea4e58267560ba42ddf169220227fc180
Paulo Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-10-26 23:26:36 -0400

    Feat: Adiciona a rota do metodo delete() - REQ23

--- src/routes/MotorcycleRouter.ts
+++ src/routes/MotorcycleRouter.ts
@@ -13,5 +13,6 @@ route.post('/', (req, res) => motorcycleController.create(req, res));
 route.get('/', (req, res) => motorcycleController.read(req, res));
 route.get('/:id', (req, res) => motorcycleController.readOne(req, res));
 route.put('/:id', (req, res) => motorcycleController.update(req, res));
+route.delete('/:id', (req, res) => motorcycleController.delete(req, res));
 
 export default route;
